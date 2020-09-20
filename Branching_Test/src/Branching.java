0cdf91a1d317665b99efc641056db55c0c0abafc
talzich <35036457+talzich@users.noreply.github.com>
2020-09-20 11:22:08 +0300

    Trying to see what only committing does.

--- Branching_Test/src/Branching.java
+++ Branching_Test/src/Branching.java
@@ -7,5 +7,9 @@ public class Branching {
 	/*
 	 * I will now try to push those lines to gitHub and delete this branch.
 	 */
-
+	
+	
+	/*
+	 * I will now make a change locally and create my branch on gitHub. 
+	 */
 }
