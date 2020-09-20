df45e34518f525b9ec559ffcc91581f8d09672f6
talzich <35036457+talzich@users.noreply.github.com>
2020-09-20 10:44:04 +0300

    Testing pulling, pushing, adding and deleting branches.

--- Branching_Test/src/Branching.java
+++ Branching_Test/src/Branching.java
@@ -1,4 +1,11 @@
 
 public class Branching {
+	
+	//Pulled this branch to local use by right clicking the repository in eclipse
+	//and clicking 'Fetch from Upstream'
+	
+	/*
+	 * I will now try to push those lines to gitHub and delete this branch.
+	 */
 
 }
