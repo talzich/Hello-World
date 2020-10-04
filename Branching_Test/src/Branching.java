2401474869a97f9ec147eeda4705c087b20adcb9
talzich <35036457+talzich@users.noreply.github.com>
2020-10-04 15:40:21 +0300

    commented print line

--- Branching_Test/src/Branching.java
+++ Branching_Test/src/Branching.java
@@ -18,7 +18,7 @@ public class Branching {
 	 */
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("change");
+		//System.out.println("Hello World!");
+		
 	}
 }
